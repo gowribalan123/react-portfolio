2e035f48b28bfebbd928d5939a07702304cc2fc7
gowribalan123 <gowribalanak@gmail.com>
2024-09-22 00:07:46 +0530

    first commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, Row, Col, Card ,Image} from 'react-bootstrap';
+import { Container, Row, Col} from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
 
 const Projects = () => {
   return (
