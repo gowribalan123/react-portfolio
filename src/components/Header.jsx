3cc7a0f50cc321343ce1e5f107887ff78da24cc7
gowribalan123 <gowribalanak@gmail.com>
2024-09-22 22:38:13 +0530

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,34 +4,49 @@ import { Nav,Button} from 'react-bootstrap';
  
 const Header = () => {
   return (
-    <Nav class="navbar navbar-expand-lg bg-body-tertiary text-light "> 
-    <div class="container bg-dark   ">
-      <a class="navbar-brand text-light " href="#">
+    <Nav className="navbar navbar-expand-lg bg-body-tertiary text-light "> 
+    <div className="container bg-dark   ">
+      <a className="navbar-brand text-light " href="#">
         Gowri Balan
-      <img src="vite.svg" width="30" height="30" class="d-inline-block align-top " alt="Profile logo" />
+      <img
+              src="vite.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top "
+              alt="Profile logo" 
+              
+            />
       </a>
-      <Button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon" />
+      <Button
+        className="navbar-toggler bg-light"
+        type="button" 
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
       </Button>
-      <div class="collapse navbar-collapse justify-content-end  " id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active text-light" aria-current="page" href="#">
+      <div className="collapse navbar-collapse justify-content-end  " id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link active text-light" aria-current="page" href="#">
               Home
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link text-light" href="#about">
+          <li className="nav-item">
+            <a className="nav-link text-light" href="#about">
               About
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link text-light" href="#projects">
+          <li className="nav-item">
+            <a className="nav-link text-light" href="#projects">
               Projects
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link text-light " href="#contact">
+          <li className="nav-item">
+            <a className="nav-link text-light " href="#contact">
               Contact
             </a>
           </li>
